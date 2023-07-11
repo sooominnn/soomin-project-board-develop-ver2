a1e99931cc77aa480f5e6219dde0f5786b05aede
sooominnn <lsm980830@gmail.com>
2023-07-11 19:45:21 +0900

    회원가입 정보 Controller로 전달

--- src/main/java/com/soomin/board/member/controller/HomeController.java
+++ src/main/java/com/soomin/board/member/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.soomin.board.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,5 +29,23 @@ public class HomeController {
     public String saveForm() {
 
         return "save";
+    }
+
+    /**
+     * 회원가입
+     *
+     * @param   memberEmail     이메일
+     * @param   memberPassword  비밀번호
+     * @param   memberName      이름
+     * @return  회원가입 결과
+     */
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+
+        return "index";
     }
 }
